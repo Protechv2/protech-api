f126ebc775503d73c588e4ff9074d3fd03e9e400
CJ-Felicitas <cedrick@apollo.com.ph>
2024-08-07 21:27:24 +0800

    first commit

---
+++ src/main/java/com/protech_api/protech_api/ProtechApiApplication.java
@@ -0,0 +1,13 @@
+package com.protech_api.protech_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtechApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protech_api/protech_api/ProtechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protech_api.protech_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
